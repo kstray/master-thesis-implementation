0398b66f86c93e5140b2b29d01da93c1a8d20e6f
Kristine Stray <kstray@stud.ntnu.no>
2022-06-06 21:02:41 +0200

    Add scripts for sampling offset and EMG signals

---
+++ generate_offset.py
@@ -0,0 +1,66 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import time
+import hackeeg
+from hackeeg import ads1299
+
+SERIAL_PORT_PATH = "/dev/cu.usbmodem142201"
+SAMPLES_PER_SECOND = ads1299.HIGH_RES_4k_SPS
+GAIN = ads1299.GAIN_12X
+CHANNELS = 8
+
+# Configure ADS1299
+hackeeg = hackeeg.HackEEGBoard(serial_port_path=SERIAL_PORT_PATH)
+hackeeg.connect()
+hackeeg.blink_board_led()
+hackeeg.sdatac()
+hackeeg.reset()
+hackeeg.disable_all_channels()
+
+# Set sampling rate
+hackeeg.wreg(ads1299.CONFIG1, SAMPLES_PER_SECOND | ads1299.CONFIG1_const)
+
+# Set internal voltage reference
+hackeeg.wreg(ads1299.CONFIG3, ads1299.PD_REFBUF | ads1299.CONFIG3_const)
+
+# Enable and configure channels for offset measurement
+for i in range(1, 9):
+    hackeeg.wreg(ads1299.CHnSET + i, ads1299.SHORTED | GAIN)
+
+
+# Measure offset continuously for 5 seconds
+hackeeg.blink_board_led()
+hackeeg.messagepack_mode()
+hackeeg.start()
+hackeeg.rdatac()
+
+print("Start")
+
+offset = []
+t_end = time.time() + 5
+while time.time() < t_end:
+    result = hackeeg.read_rdatac_response()
+    if result:
+        offset.append(result)
+    else:
+        print("no data to decode")
+        print(f"result: {result}")
+
+print("Sampling complete")
+
+hackeeg.sdatac()
+hackeeg.stop()
+
+
+# Process offset data
+channel_offset = np.empty((CHANNELS, len(offset)))
+for i, s in enumerate(offset):
+    dataKey = s.get(hackeeg.MpDataKey)
+    if dataKey:
+        data = s.get('channel_data')
+        for channel in range(CHANNELS):
+            channel_offset[channel, i] = data[channel]
+channel_offset = channel_offset.astype(np.float64)
+channel_offset = np.median(channel_offset, axis=1)
+np.save('channel_offset.npy', channel_offset)
---
+++ sample_emg.py
@@ -0,0 +1,82 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import time
+import hackeeg
+from hackeeg import ads1299
+
+SERIAL_PORT_PATH = "/dev/cu.usbmodem142201"
+SAMPLES_PER_SECOND = ads1299.HIGH_RES_4k_SPS
+GAIN = ads1299.GAIN_12X
+CHANNELS = 8
+
+# Configure ADS1299
+hackeeg = hackeeg.HackEEGBoard(serial_port_path=SERIAL_PORT_PATH)
+hackeeg.connect()
+hackeeg.blink_board_led()
+hackeeg.sdatac()
+hackeeg.reset()
+hackeeg.disable_all_channels()
+
+# Set sampling rate
+hackeeg.wreg(ads1299.CONFIG1, SAMPLES_PER_SECOND | ads1299.CONFIG1_const)
+
+# Enable and configure channels
+for i in range(1, 9):
+   hackeeg.wreg(ads1299.CHnSET + i, ads1299.ELECTRODE_INPUT | GAIN)
+
+# Differential mode - each channel is the differential voltage between adjacent electrodes
+hackeeg.wreg(ads1299.MISC1, ads1299.MISC1_const)
+# For single-ended mode, uncomment the following line to set the SRB1 bit,
+# which sends mid-supply voltage to the N inputs
+# hackeeg.wreg(ads1299.MISC1, ads1299.SRB1 | ads1299.MISC1_const)
+
+# Choose bias as an average of all channels but channel 8
+# hackeeg.wreg(ads1299.BIAS_SENSP, 0b11111111)
+# hackeeg.wreg(ads1299.BIAS_SENSN, 0b11111111)
+# Use internal BIASREF signal source and turn on bias amplifier
+# route BIASOUT to bias electrode: JP9: 1-2, JP6: NC (not connected)
+# route BIASOUT to BIASIN: JP9: 2-3
+hackeeg.wreg(ads1299.CONFIG3, ads1299.PD_REFBUF | ads1299.BIASREF_INT | ads1299.PD_BIAS | ads1299.CONFIG3_const)
+# hackeeg.wreg(ads1299.CONFIG3, ads1299.PD_REFBUF | ads1299.CONFIG3_const)
+
+
+# Read data continuously for 5 seconds
+hackeeg.blink_board_led()
+hackeeg.messagepack_mode()
+hackeeg.start()
+hackeeg.rdatac()
+
+print("Flex!")
+
+samples = []
+t_end = time.time() + 5
+while time.time() < t_end:
+    result = hackeeg.read_rdatac_response()
+    if result:
+        samples.append(result)
+    else:
+        print("no data to decode")
+        print(f"result: {result}")
+
+print("Sampling complete")
+
+hackeeg.sdatac()
+hackeeg.stop()
+
+
+# Process samples
+channel_data = np.empty((9, len(samples)))
+for i, s in enumerate(samples):
+    dataKey = s.get(hackeeg.MpDataKey)
+    if dataKey:
+        timestamp = s.get('timestamp')
+        data = s.get('channel_data')
+        channel_data[0, i] = timestamp
+        for channel in range(1,9):
+            channel_data[channel, i] = data[channel-1]
+channel_data = channel_data.astype(np.float64)
+
+channel_offset = np.load('channel_offset.npy')
+channel_data[1:] = channel_data[1:] - channel_offset[...,np.newaxis]
+np.save('channel_data.npy', channel_data)
