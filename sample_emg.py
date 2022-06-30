c5a0e569964b5fd595d3a4999074a77abe4977ba
Kristine Stray <kstray@stud.ntnu.no>
2022-06-30 20:01:59 +0200

    Sample external bias

--- sample_emg.py
+++ sample_emg.py
@@ -23,18 +23,19 @@ hackeeg.disable_all_channels()
 hackeeg.wreg(ads1299.CONFIG1, SAMPLES_PER_SECOND | ads1299.CONFIG1_const)
 
 # Enable and configure channels
-for i in range(1, 9):
-   hackeeg.wreg(ads1299.CHnSET + i, ads1299.ELECTRODE_INPUT | GAIN)
+for i in range(2, 9):
+   hackeeg.wreg(ads1299.CHnSET + i, ads1299.SHORTED | ads1299.PDn)
+hackeeg.wreg(ads1299.CHnSET + 1, ads1299.ELECTRODE_INPUT | GAIN)
 
 # Differential mode - each channel is the differential voltage between adjacent electrodes
-# hackeeg.wreg(ads1299.MISC1, ads1299.MISC1_const)
+hackeeg.wreg(ads1299.MISC1, ads1299.MISC1_const)
 # For single-ended mode, uncomment the following line to set the SRB1 bit,
 # which sends mid-supply voltage to the N inputs
-hackeeg.wreg(ads1299.MISC1, ads1299.SRB1 | ads1299.MISC1_const)
+# hackeeg.wreg(ads1299.MISC1, ads1299.SRB1 | ads1299.MISC1_const)
 
 # Choose bias as an average of all channels but channel 8
-hackeeg.wreg(ads1299.BIAS_SENSP, 0b11111111)
-hackeeg.wreg(ads1299.BIAS_SENSN, 0b11111111)
+# hackeeg.wreg(ads1299.BIAS_SENSP, 0b11111111)
+# hackeeg.wreg(ads1299.BIAS_SENSN, 0b11111111)
 # Use internal BIASREF signal source and turn on bias amplifier
 # route BIASOUT to bias electrode: JP9: 1-2, JP6: NC (not connected)
 # route BIASOUT to BIASIN: JP9: 2-3
@@ -50,19 +51,19 @@ hackeeg.rdatac()
 
 print("Start")
 samples = []
-timestamp = []
+# timestamp = []
 t_end = time.time() + 20
-start = datetime.datetime.now()
-last = start
+# start = datetime.datetime.now()
+# last = start
 while time.time() < t_end:
     result = hackeeg.read_rdatac_response()
     if result:
         samples.append(result)
-        now = datetime.datetime.now()
-        if ((now - last) > datetime.timedelta(seconds=3)):
-            print("now")
-            last = now
-            timestamp.append(result.get('timestamp'))
+        # now = datetime.datetime.now()
+        # if ((now - last) > datetime.timedelta(seconds=3)):
+        #     print("now")
+        #     last = now
+        #     timestamp.append(result.get('timestamp'))
     else:
         print("no data to decode")
         print(f"result: {result}")
@@ -86,6 +87,6 @@ for i, s in enumerate(samples):
             channel_data[channel, i] = data[channel-1]
 channel_data = channel_data.astype(np.float64)
 
-channel_offset = np.load('channel_offset.npy')
+channel_offset = np.load('data/channel_offset.npy')
 channel_data[1:] = channel_data[1:] - channel_offset[...,np.newaxis]
-np.save('channel_data_single_biassense.npy', channel_data)
+np.save('channel_data_external_bias.npy', channel_data)
